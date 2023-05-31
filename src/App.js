1c1b0fa2bafdeeed50611ed8e23fa03438dc0a04
anjana <anjana@vuedata.in>
2023-05-31 07:53:47 +0530

    Add search and movies load

--- src/App.js
+++ src/App.js
@@ -8,13 +8,30 @@ const SEARCHAPI =
   "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
 export default function App() {
-  const [movies, setMovies] = useState([1, 2, 3]);
+  const [movies, setMovies] = useState([]);
   const [search, setSearch] = useState("");
   const getAllMovies = () => {
     axios
       .get(APIURL)
       .then((response) => {
-        console.log(response.data.results)
+        setMovies(response.data.results);
+        console.log(movies);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const changeTheSearch = (event) => {
+    console.log(event.target.value);
+    setSearch(event.target.value);
+  };
+
+  const getSearchedMovies = (event) => {
+    axios
+      .get(SEARCHAPI + search)
+      .then((response) => {
+        setMovies(response.data.results);
       })
       .catch((error) => {
         console.log(error);
@@ -22,19 +39,22 @@ export default function App() {
   };
 
   useEffect(() => {
+    setMovies([]);
     if (search === "") {
-      getAllMovies()
+      getAllMovies();
     } else {
-     // getSearchedMovies();
+      getSearchedMovies();
     }
   }, [search]);
   return (
-    <div className="max-w-[1240px] shadow-xl h-full mx-auto p-3">
+    <div className="max-w-[1240px] shadow-xl h-[400px]  mx-auto p-3">
       <input
         type="search"
-        className="w-full border border-black p-4 rounded text-slate-400"
+        value={search}
+        onChange={changeTheSearch}
+        className="w-full border border-black p-4 rounded text-slate-700"
       ></input>
-      <Result movies={movies} />
+      {movies.length === 0 ? <div className="text-3xl text-center mt-2">Loading ....</div> : <Result movies={movies} />}
     </div>
   );
 }
--- src/components/Result.js
+++ src/components/Result.js
@@ -1,21 +1,21 @@
 import React from "react";
 
 export default function Result(props) {
-  const boxes = props.movies.map(() => {
-    return <Box />;
+  const boxes = props.movies.map((item, index) => {
+    return <Box key={index} image={item.poster_path} title={item.original_title} rating={item.vote_average} />;
   });
 
   return <div className="w-full grid grid-cols-4 gap-5">{boxes}</div>;
 }
 
-const Box = () => {
+const Box = (props) => {
   const IMGPATH = "https://image.tmdb.org/t/p/w1280";
   return (
-    <div className="shadow min-h-[200px] border border-black mt-3">
-        <img src="https://picsum.photos/id/237/200/300" className="w-full"></img>
+    <div className="shadow min-h-[200px]  mt-3 pb-1">
+        <img src={IMGPATH+props.image} className="w-full"></img>
         <div className="flex justify-between px-2 items-center">
-            <span className="text-2xl">TITLE HERE</span>
-            <span className="text-xl text-yellow-500 font-bold">7.4</span>
+            <span className="text-2xl">{props.title}</span>
+            <span className="text-xl text-yellow-500 font-bold">{props.rating}</span>
         </div>
     </div>
   )
